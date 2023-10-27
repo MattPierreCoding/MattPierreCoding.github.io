446ec519a54271d6994168852cb8c6b8815b8076
swacynubs <rcb4sc10@gmail.com>
2023-10-27 00:22:01 -0400

    fi9rst comit

--- austin.py
+++ austin.py
@@ -9,6 +9,8 @@ for i in range(n-2):
     default = default2
     default2 = total
     print(total)
-            
-
+num = 2           
+for i in range(0,11):
+    print(num)
+    num = num * 2
 # print(total)
